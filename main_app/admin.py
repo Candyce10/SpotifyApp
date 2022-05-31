895039022592a5d4cdb41643c1a0db5d28cfa2d0
Candyce10 <candycematthews@yahoo.com>
2022-05-31 15:02:43 -0700

    CRUD complete

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Artist # import the Artist model from models.py
+
+admin.site.register(Artist) # this line will add the model to the admin panel
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,13 @@
+from .models import Artist
 from django.shortcuts import render
 from django.views import View # handles requests
 from django.http import HttpResponse # handles sending a type of response
 from django.views.generic.base import TemplateView
+# This will import the class we are extending 
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
 
 # Create your views here.
 
@@ -25,3 +31,50 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+
+class ArtistList(TemplateView):
+    template_name = "artist_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # to get the query parameter we have to acccess it in the request.GET dictionary object        
+        name = self.request.GET.get("name")
+        # If a query exists we will filter by name 
+        if name != None:
+            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
+            context["artists"] = Artist.objects.filter(name__icontains=name)
+              # We add a header context that includes the search param
+            context["header"] = f"Searching for '{name}'"
+        else:
+            context["artists"] = Artist.objects.all()# this is where we add the key into our context object for the view to use
+             # default header for not searching 
+            context["header"] = "Trending Artists"
+        return context
+
+
+class ArtistCreate(CreateView):
+    model = Artist
+    fields = ['name', 'img', 'bio', 'verified_artist']
+    template_name = "artist_create.html"
+    success_url = "/artists/"
+
+class ArtistDetail(DetailView):
+    model = Artist
+    template_name = "artist_detail.html"
+
+class ArtistUpdate(UpdateView):
+    model = Artist
+    fields = ['name', 'img', 'bio', 'verified_artist']
+    template_name = "artist_update.html"
+    success_url = "/artists/"
+
+    def get_success_url(self):
+        return reverse('artist_detail', kwargs={'pk': self.object.pk})#returns to artist page
+
+class ArtistDelete(DeleteView):
+    model = Artist
+    template_name = "artist_delete_confirmation.html"
+    success_url = "/artists/"
+
+    
